cda69ede6d8aef17412eb96d5e76ed899e3888f7
Anna Nayara Lira <anna_nayara@hotmail.com>
2023-03-27 10:33:17 -0300

    barra lateral e botao

---
+++ src/components/BarraLatera.vue
@@ -0,0 +1,54 @@
+<template>
+    <header>
+      <h1>
+        <img src="../assets/logo.png" alt="logo do alura tracker" />
+      </h1>
+      <button class="button" @click="alterarTema">
+        {{ textoBotao }}
+      </button>
+    </header>
+  </template>
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    name: "BarraLateral",
+    emits: ["aoTemaAlterado"],
+    data() {
+      return {
+        modoEscuroAtivo: false,
+      };
+    },
+    computed: {
+      textoBotao() {
+        if (this.modoEscuroAtivo) {
+          return "Desativar modo escuro";
+        }
+        return "Ativar modo escuro";
+      },
+    },
+    methods: {
+      alterarTema(): void {
+        this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
+        this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      },
+    },
+  });
+  </script>
+  <style scoped>
+  header {
+    padding: 1rem;
+    background-color: var(--barra-primaria);
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+  }
+  h1 {
+    margin-bottom: 32px;
+  }
+  @media only screen and (max-width: 768px) {
+    header {
+      padding: 2.5rem;
+      height: auto;
+    }
+  }
+  </style>
---
+++ src/components/Botao.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="button" @click="clickButton" :disabled="disabled">
+      <span class="icon">
+        <i :class="icon"></i>
+      </span>
+      <span>{{ text }}</span>
+    </button>
+  </template>
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    name: "Button-Control",
+    emits: ["clickButton"],
+    props: {
+      disabled: {
+        type: Boolean,
+      },
+      icon: {
+        type: String,
+        required: true,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+    },
+    methods: {
+      clickButton(): void {
+        this.$emit("clickButton");
+      },
+    },
+  });
+  </script>
+  
